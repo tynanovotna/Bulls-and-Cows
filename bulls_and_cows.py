b66608c97ab9da6a7f4b0a5010f488b0151ea5a9
Kristýna Novotná <tynadanielova@seznam.cz>
2022-08-02 07:35:59 -0700

    WIP

---
+++ bulls_and_cows.py
@@ -0,0 +1,10 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Kristýna Novotná
+email: tynadanielova@seznam.cz
+discord: KristýnaN #4503
+"""
+
+from random import randint
+
