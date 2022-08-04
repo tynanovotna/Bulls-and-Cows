b3bddf315fbd2a95bf26a93a16bcc517bf6e060a
Kristýna Novotná <tynadanielova@seznam.cz>
2022-08-04 09:14:47 -0700

    small changes and added players input, rules

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -9,12 +9,60 @@ discord: KristýnaN #4503
 import random
 import time
 
-SEPARATOR = "-" * 47
+SEPARATOR = "-" * 50
+GAME_RULES = """
+Game goal is guessing the 4 digit number.
+Right number and position is called Bull/s.
+Right number but false position is called Cow/s.""" 
 
 def playing_game():
     start_time = time.time()
     number = hidden_number()
     count = 1
+    while True:
+        players_number = players_input()
+        bulls, cows = bulls_and_cows_count(number, players_number)
+        bull, cow = guesing_result(bulls, cows)
+        if bulls < 4:
+            print(f"{bulls} {bull}, {cows} {cow}.")
+            print(SEPARATOR)
+        end_game = check_game_end(bulls)
+        if end_game:
+            break
+        count += 1 
+    end_time = time.time()
+    total_time = end_time - start_time
+    minutes = int(total_time / 60)
+    seconds = int(total_time % 60)
+    if minutes < 1:
+        phrase = "Amazing"
+    elif minutes in range(1, 2):
+        phrase = "Quite good"
+    elif minutes in range(2, 4):
+        phrase = "Average"
+    elif minutes in range(4, 6):
+        phrase = "not so good"
+    else:
+        phrase = "very bad"
+    print(SEPARATOR)
+    print(f"Correct, you've guessed the right number in {count} \nguesses, {minutes} minutes & {seconds} seconds!")
+    print(f"That's {phrase}!")
+    print(SEPARATOR)
+    play_again()
+       
+def welcome_player():
+    print("Hi there!")
+    print(SEPARATOR)
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(SEPARATOR)
+    show_game_rules = input("Would you like to see Bulls and Cows rules? Y/N ").lower()
+    if show_game_rules in ["y", "yes"]:
+        print(GAME_RULES)
+    print(SEPARATOR)
+
+
+def players_input():
     while True:
         players_number = input("Enter a number: ")
         if not players_number.isnumeric():
@@ -25,38 +73,22 @@ def playing_game():
             print("Enter only 4 digit number. Try again.")
             print(SEPARATOR)
             continue
-        bulls, cows = bulls_and_cows_count(number, players_number)
-        bull, cow = guesing_result(bulls, cows)
-        print(f"{bulls} {bull}, {cows} {cow}.")
-        print(SEPARATOR)
-        end_game = check_game_end(bulls)
-        if end_game:
+        no_duplicates = set()
+        for number in players_number:
+            no_duplicates.add(number)
+        if len(no_duplicates) < 4:
+            print("Duplicit numbers. Try again.")
+            print(SEPARATOR)
+        else:
             break
-        count += 1 
-    end_time = time.time()
-    total_time = end_time - start_time
-    minutes = int(total_time / 60)
-    seconds = int(total_time % 60)
-    print(SEPARATOR)
-    print(f"Correct, you've guessed the right number in {count} \nguesses, {minutes} minutes & {seconds} seconds!")
-    print("That's Amazing!")
-    print(SEPARATOR)
-    play_again()
-    return players_number
+    return players_number        
 
-def welcome_player():
-    print("Hi there!")
-    print(SEPARATOR)
-    print("I've generated a random 4 digit number for you.")
-    print("Let's play a bulls and cows game.")
-    print(SEPARATOR)
-    
 def play_again():
     another_game = input("Would you like to play another game? Y/N ").lower()
     print(SEPARATOR)
-    if another_game == "y" or another_game == "yes":
+    if another_game in ["y", "yes"]:
         playing_game()
-    elif another_game == "n" or another_game == "no":
+    elif another_game in ["n", "no"]:
         pass
     else:
         print("Wrong answer, try again.")
@@ -98,4 +130,3 @@ def hidden_number():
 
 welcome_player()
 playing_game()
-
